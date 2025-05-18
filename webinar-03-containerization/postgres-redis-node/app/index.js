1fa1f4c5bd1afde97deda38a065255084c1cd418
Tomasz Ducin <tomasz@ducin.dev>
2025-05-19 01:31:14 +0200

    enhancements

--- webinar-03-containerization/postgres-redis-node/app/index.js
+++ webinar-03-containerization/postgres-redis-node/app/index.js
@@ -1,63 +1,32 @@
-const express = require('express');
-const { Pool } = require('pg');
-const redis = require('redis');
 const fs = require('fs');
+const express = require('express');
+const redis = require('redis');
 
-// Function to check required environment variables
-const checkEnvVariables = () => {
-  const requiredEnvVars = [
-    'NODE_APP_PORT',
-    'REDIS_PASSWORD_FILE',
-    'DB_PASSWORD_FILE',
-    'DB_USER',
-    'DB_HOST',
-    'DB_NAME',
-    'DB_PORT',
-    'REDIS_HOST',
-    'REDIS_PORT'
-  ];
+const { invokeMemoryLeak } = require('./memory-leak');
+const { assertEnvVars } = require('./env');
+const pool = require('./database');
+const redisClient = require('./redis');
 
-  const missingVars = requiredEnvVars.filter(variable => !process.env[variable]);
-
-  if (missingVars.length > 0) {
-    throw new Error(`Missing required environment variables: ${missingVars.join(', ')}`);
-  }
-};
-
-// Check environment variables
-checkEnvVariables();
-
-const redisPassword = fs.readFileSync(process.env.REDIS_PASSWORD_FILE, 'utf8').trim();
-const dbPassword = fs.readFileSync(process.env.DB_PASSWORD_FILE, 'utf8').trim();
-const redisUrl = `redis://:${redisPassword}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
-
-console.log('[debug]', process.env, {redisPassword, redisUrl})
-
-// PostgreSQL Connection
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: dbPassword,
-  port: process.env.DB_PORT,
-});
-
-// Redis Connection
-const redisClient = redis.createClient({
-  url: redisUrl
-});
-
-// Connect to Redis
-(async () => {
-  await redisClient.connect();
-  console.log('Connected to Redis');
-})().catch(err => console.error('Redis connection error:', err));
+assertEnvVars(
+  'NODE_APP_PORT',
+  'SIMULATE_MEMORY_LEAK',
+  'REDIS_PASSWORD_FILE',
+  'DB_PASSWORD_FILE',
+  'DB_USER',
+  'DB_HOST',
+  'DB_NAME',
+  'DB_PORT',
+  'REDIS_HOST',
+  'REDIS_PORT'
+);
 
 const app = express();
 const port = process.env.NODE_APP_PORT;
 
 // GET /products endpoint
 app.get('/products', async (req, res) => {
+  invokeMemoryLeak();
+
   try {
     // Try to get products from Redis cache
     const cachedProducts = await redisClient.get('products');
